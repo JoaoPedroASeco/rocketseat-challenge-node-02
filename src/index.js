930d0405515a547520e39cb10f3bc925356528c0
dev.jpedro <dev.jpedro@gmail.com>
2023-01-05 11:22:07 -0300

    challenge finished

--- src/index.js
+++ src/index.js
@@ -9,20 +9,70 @@ app.use(cors());
 
 const users = [];
 
-function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+function checksExistsUserAccount(request, response, next) { // FEITO
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'Unexistent user' });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.pro === false && user.todos.length < 10) {
+    next()
+  } else if (user.pro === true) {
+    next()
+  } else if(user.pro === false && user.todos.length >= 10) {
+    return response.status(403).json({ error: 'Error' });
+  }
 }
 
-function checksTodoExists(request, response, next) {
-  // Complete aqui
+function checksTodoExists(request, response, next) { // FEITO
+  const { username } = request.headers;
+  const { id } = request.params;
+  
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "Unexistent user" });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "This is not a UUID" });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({ error: "Unexistent todo" });
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  next();
 }
 
-function findUserById(request, response, next) {
-  // Complete aqui
+function findUserById(request, response, next) {  // FEITO
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "Unexistent user" });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
@@ -92,8 +142,12 @@ app.put('/todos/:id', checksTodoExists, (request, response) => {
   const { title, deadline } = request.body;
   const { todo } = request;
 
+  const test = { title, deadline, todo }
+
   todo.title = title;
   todo.deadline = new Date(deadline);
+
+  const todo2 = todo
 
   return response.json(todo);
 });
